237641ad6547a5e36419ea9ed416f62a793cbe90
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-11 16:50:48 -0700

    First Commit

---
+++ Project2/src/ModifiedProgram.java
@@ -0,0 +1,11 @@
+
+public class ModifiedProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}
+
+}
