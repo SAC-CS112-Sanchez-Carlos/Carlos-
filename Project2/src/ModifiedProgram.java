5159f2e142ea4b17e71e5df8f32e9ddbe2c11b29
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-11 19:35:31 -0700

    Commit 3 - program compares two numbers

--- Project2/src/ModifiedProgram.java
+++ Project2/src/ModifiedProgram.java
@@ -1,7 +1,7 @@
 //Displaying multiple lines of text with a single statement
 
 
-import java.util.Scanner;
+import java.util.Scanner; //program uses class Scanner
 
 public class ModifiedProgram {
 
@@ -10,9 +10,7 @@ public class ModifiedProgram {
 		Scanner input = new Scanner( System.in );
 		
 		int number1; //first number to add
-		int number2; //second number to add
-		int sum; //sum of number1 and number2
-	
+		int number2; //second number to add	
 		
 		System.out.print("Enter your first integer: "); //prompt
 		number1 = input.nextInt(); //read first number from user
@@ -20,10 +18,23 @@ public class ModifiedProgram {
 		System.out.print("Enter your second integer: "); //prompt
 		number2 = input.nextInt(); //read second number from user
 		
-		sum = number1 + number2;
+		if ( number1 == number2 )
+			System.out.printf("%d == &d\n", number1, number2); //display number1 and number2
 		
-		System.out.printf("Sum is %d\n", sum); //display sum of number1 and number2
-	
+		if (number1 != number2)
+			System.out.printf("%d != %d\n", number1, number2);
+		
+		if (number1 < number2)
+			System.out.printf("%d < %d\n", number1, number2);
+		
+		if (number1 > number2)
+			System.out.printf("%d > %d\n", number1, number2);
+		
+		if (number1 <= number2)
+			System.out.printf("%d <= %d\n", number1, number2);
+		
+		if (number1 >= number2)
+			System.out.printf("%d >= %d\n", number1, number2);
 	}
 
 }
