9e413e5c8b5f6031087b5067d2c9b22d2e041cca
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-11 17:56:43 -0700

    changed program to addition of two numbers

--- Project2/src/ModifiedProgram.java
+++ Project2/src/ModifiedProgram.java
@@ -1,12 +1,29 @@
-//printing multiple lines of text with a single statement
+//Displaying multiple lines of text with a single statement
 
+
+import java.util.Scanner;
 
 public class ModifiedProgram {
 
 	public static void main(String[] args) {
 
-		System.out.print("Welcome to ");
-		System.out.println("Welcome\nto\nJava\nProgramming!");
+		Scanner input = new Scanner( System.in );
+		
+		int number1; //first number to add
+		int number2; //second number to add
+		int sum; //sum of number1 and number2
+	
+		
+		System.out.print("Enter your first integer: "); //prompt
+		number1 = input.nextInt(); //read first number from user
+		
+		System.out.print("Enter your second integer: "); //prompt
+		number2 = input.nextInt(); //read second number from user
+		
+		sum = number1 + number2;
+		
+		System.out.printf("Sum is %d\n", sum); //display sum of number1 and number2
+	
 	}
 
 }
