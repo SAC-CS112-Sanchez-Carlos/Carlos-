5f60dba3477059175071f8751fcd63942850cc19
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-13 21:00:50 -0700

    First Commit

---
+++ Gradebook/src/Gradebook.java
@@ -0,0 +1,14 @@
+//Gradebook.java
+//Class decleration with one method
+
+
+public class Gradebook {
+
+	//display a weslcome message to the Gradebook user
+	public void displayMessage(){
+
+		System.out.println("Welcome to the Gradebook!");
+	}
+	//end method displayMessage
+//end class Gradebook
+}
---
+++ Gradebook/src/GradebookTest.java
@@ -0,0 +1,17 @@
+//GradebookTest.java
+//Creating a Gradebook object and calling its displayMessage method
+
+public class GradebookTest 
+{
+	//main method begins program execution
+	public static void main(String[] args) 
+	{
+		//create a Gradebook object and assign it to myGradebook
+		Gradebook myGradebook = new Gradebook();
+		
+		//call myGradebook's displayMessage method
+		myGradebook.displayMessage();
+
+	}//end main
+
+}//end class GradebookTest
