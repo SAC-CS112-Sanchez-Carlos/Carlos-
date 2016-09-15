688a56cc2a85f0aeeb0f33c38af198991ec6fbb6
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-14 21:12:40 -0700

    Test 1 - Name and Retire Program

---
+++ Test1/src/Retire.java
@@ -0,0 +1,12 @@
+
+public class Retire {
+
+	public static void main(String[] args) {
+
+		String Name = "Carlos Sanchez.";
+		int date = 37;
+		
+		System.out.printf("\n%s%s\n%s%d%s", "My Name is ", Name, "I will retire in ", date, " years.");
+	}
+
+}
