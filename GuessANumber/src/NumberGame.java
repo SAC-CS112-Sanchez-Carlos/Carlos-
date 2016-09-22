55112c9885f1a3d2f0456ee1c8eeea7ba6316fe6
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-21 21:46:53 -0700

    commit (pre while and do while)

--- GuessANumber/src/NumberGame.java
+++ GuessANumber/src/NumberGame.java
@@ -7,30 +7,35 @@ public class NumberGame {
 
 		int computerNumber; // assigns computer's number
 		int playerGuess; // assigns player's number
+		int replay = 0; // number of times the game will be replayed
 		String playerInput; // allows for input into the variable playerInput
 		
-		computerNumber = 1 + (int) (Math.random() * 10);// creates a random number between 1 and 10
+		computerNumber = 1 + (int) (Math.random() * 9);// creates a random number between 1 and 10
 		
 		// Welcome to the game message and ask player for their guess
 		System.out.printf("%s\n%s\n\n%s", "Welcome to Guess a Number Game!",
 				"I'm thinking of a number between 1 and 10!", "Take a guess? "); 
 
 		Scanner input = new Scanner(System.in); // create a scanner object to obtain input from the user
-
+		
+		for (replay=0; replay<4; replay=replay+1) //allow for player to have 3 guesses
+		
+		{
 		playerGuess = input.nextInt(); // assigns player's input to the playerGuess variable
 
 		if ((playerGuess > 0) && (playerGuess <11) && (playerGuess == computerNumber)) {
 			System.out.println("You guessed right! You win"); //player's guess is between 1 and 10 and is equal to the random number
 	}
 		else if ((playerGuess < 0) || (playerGuess > 10)){
-			System.out.println("You didn't guess a number between 1 and 10."); //prints if player's guess was not between 1 and 10
+			System.out.println("You didn't guess a number between 1 and 10.\r"); //prints if player's guess was not between 1 and 10
 	}
 		else if ((playerGuess > 0) && (playerGuess <11) && (playerGuess < computerNumber)){
-			System.out.println("You guessed wrong. Try a higher number next time."); //player's guess is between 1 and 10 and is less than the random number
+			System.out.println("You guessed wrong. Try a higher number next time.\r"); //player's guess is between 1 and 10 and is less than the random number
 	}
 		else if ((playerGuess > 0) && (playerGuess <11) && (playerGuess > computerNumber)){
-			System.out.println("You guessed wrong. Try a lower number next time."); //player's guess is between 1 and 10 and is more than the random number
+			System.out.println("You guessed wrong. Try a lower number next time.\r"); //player's guess is between 1 and 10 and is more than the random number
 	}
+		}		
 		System.out.printf("\nThe number was " + computerNumber); //prints out computerNumber
 		
 		System.out.println("\n\nWould you like to play again?\nIf yes, enter Y");	//asks player if they want to play again and asks for input
