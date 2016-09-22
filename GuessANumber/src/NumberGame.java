8ca57dc5cab5c44eba29ce4a1a76be4127dd18cf
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-09-22 11:41:31 -0700

    commit 8

--- GuessANumber/src/NumberGame.java
+++ GuessANumber/src/NumberGame.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class NumberGame {
 
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 
-		int computerNumber; // assigns computer's number
-		int playerGuess; // assigns player's number
-		int replay = 0; // number of times the game will be replayed
-		String playerInput = "Y"; // allows for input into the variable playerInput
+		int computerNumber = 0; // assigns computer's number
+		int playerGuess = 0; // assigns player's number
+		int replay = 1; // assigns 0 to the variable replay
+		String playerInput = "Y"; // assigns "Y" to the variable playerInput
 		
 		computerNumber = 1 + (int) (Math.random() * 9);// creates a random number between 1 and 10
 		
 		do { //runs the program again as long as the player input Y
 			
-		
-		// Welcome to the game message and ask player for their guess
-		System.out.printf("%s\n%s\n\n%s", "Welcome to Guess a Number Game!",
+		System.out.printf("%s\n%s\n\n%s%n", "Welcome to Guess a Number Game!", // Welcome to the game message and ask player for their guess
 				"I'm thinking of a number between 1 and 10!", "Take a guess? "); 
 
 		Scanner input = new Scanner(System.in); // create a scanner object to obtain input from the user
@@ -25,7 +23,7 @@ public class NumberGame {
 		playerGuess = input.nextInt(); // assigns player's input to the playerGuess variable
 
 		if ((playerGuess > 0) && (playerGuess <11) && (playerGuess == computerNumber)) {
-			System.out.println("You guessed right! You win"); //player's guess is between 1 and 10 and is equal to the random number
+			System.out.println("You guessed right! You win!"); //5player's guess is between 1 and 10 and is equal to the random number
 			break;
 		}
 		else if ((playerGuess < 0) || (playerGuess > 10)){
@@ -37,21 +35,28 @@ public class NumberGame {
 		else if ((playerGuess > 0) && (playerGuess <11) && (playerGuess > computerNumber)){
 			System.out.println("You guessed wrong. Try a lower number next time.\r"); //player's guess is between 1 and 10 and is more than the random number
 	}
-		replay = replay +1; 
-		
+		replay = replay +1;  // adds 1 to the replay counter
 		}
-		System.out.printf("\nThe number was " + computerNumber); //prints out computerNumber
+		
+		System.out.printf("\nThe number was " + computerNumber + "."); //prints out computerNumber
 		
 		System.out.println("\n\nWould you like to play again?\nIf yes, enter Y");	//asks player if they want to play again and asks for input
 		playerInput = input.next(); //assigns player's input into the variable playerInput
 		
 		if (playerInput.equalsIgnoreCase("Y")){
-		System.out.println("Let's play again!\n"); //prints if player inputs Y
+		System.out.println("Let's play again!\n\n************************************************\n"); //prints if player inputs Y
 		replay = 1;
+		try { 
+			Thread.sleep(1500);
+		}
+		catch(InterruptedException ex)
+		{
+
+		}
 		}
 		
-		else System.out.println("Goodbye.");//prints if player inputs something other than Y
+		if (!playerInput.equalsIgnoreCase("Y")) System.out.println("Goodbye.");//prints if player inputs something other than Y
 		}
-		while (playerInput.equalsIgnoreCase("Y"));
+		while (playerInput.equalsIgnoreCase("Y")); //continues the program while plater input is equal to "Y"
 	}
 }
