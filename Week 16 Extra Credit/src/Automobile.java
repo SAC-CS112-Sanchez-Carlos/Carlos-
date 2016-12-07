6af3bd8ccb18a7a13362f747d673eb9e26718a35
SAC-CS112-Sanchez-Carlos <appleguy1@gmail.com>
2016-12-07 14:12:48 -0800

    commit 2

---
+++ Week 16 Extra Credit/src/Automobile.java
@@ -0,0 +1,125 @@
+public class Automobile implements Runnable, E {
+
+	public int m_year = 0, m_gear = 0, m_operatingCycle = 0, m_currentSpeed = 0, m_currentGear = 0;
+	public String m_make = "", m_model = "", m_color = "", m_name = "";
+	public float m_distanceTraveled = 0;
+
+	/*
+	 * constructor automobile with parameters year, make, model, color, name,
+	 * and gear
+	 */
+	public Automobile(int year, String make, String model, String color, String name, int gear) {
+		m_year = year;
+		m_make = make;
+		m_model = model;
+		m_name = name;
+		m_gear = gear;
+		m_color = color;
+	}
+
+	public void setYear(int year) { // allows user to set car's year
+		m_year = year;
+	}
+
+	public void setMake(String make) { // allows user to set car's make
+		m_make = make;
+	}
+
+	public void setModel(String model) { // allows user to set car's model
+		m_model = model;
+	}
+
+	public void setColor(String color) { // allows user to set car's color
+		m_color = color;
+	}
+
+	public void setName(String name) { // allows user to set car's name
+		m_name = name;
+	}
+
+	public void setGears(int gear) { // allows user to set car's gears
+		m_gear = gear;
+	}
+
+	public void setOperatingCycle(int operatingCycle) { // sets operating cycle,
+														// current speed, and
+														// distance traveled of
+														// car
+
+		if (operatingCycle <= m_gear)
+			m_currentGear = operatingCycle;
+
+		m_currentSpeed = m_currentGear * 10;
+
+		m_distanceTraveled += m_currentSpeed;
+	}
+
+	public int getYear() { // allows user to set car's year
+		return m_year;
+	}
+
+	public String getMake() { // allows user to get make of car
+		return m_make;
+	}
+
+	public String getModel() { // allows user to get model of car
+		return m_model;
+	}
+
+	public String getColor() { // allows user to get color of car
+		return m_color;
+	}
+
+	public String getName() { // allows user to get name of car
+		return m_name;
+	}
+
+	public int getGear() { // allows user to get numer of gears of car
+		return m_gear;
+	}
+
+	public int getOperatingCycle() { // allows user to get operating cycle
+		return m_operatingCycle;
+	}
+
+	public int currentGear() { // returns car's currentGear
+		if (m_operatingCycle > m_gear) { // 4>3
+			m_currentGear = m_gear;
+		} else {
+			m_currentGear = m_operatingCycle;
+		}
+		return m_currentGear;
+	}
+
+	public int getSpeed() { // returns car's current speed
+		return m_currentSpeed;
+	}
+
+	public float getDistanceTraveled() throws Exception { // returns distance
+															// traveled
+		if (m_distanceTraveled < 10)
+			System.out.println(m_name + " goes Prrr.");
+		else
+			System.out.println(m_name + " goes Vroom.");
+		if (m_distanceTraveled > 100) { //changed 100 to 1000
+			System.out.println(m_name + " makes a Sputter...");
+			throw new Exception();
+		}
+
+		return m_distanceTraveled;
+	}
+
+	public String toString() { // prints string of info based on Object
+		return "\n" + "Name: " + m_name + "\nYear: " + m_year + "\nMake: " + m_make + "\nModel: " + m_model
+				+ "\nColor: " + m_color + "\nNumber of gears: " + m_gear;
+
+	}
+
+	
+	public void run() {
+		String info = "Car is a " + m_color + " " + m_year + " " + m_make + " " + m_model + " nicknamed " + m_name + ". It has " + m_gear + " gears.";
+			System.out.println(info);
+			System.out.println(m_name + "'s distance = " + m_distanceTraveled + ".\n");
+		}
+	}
+
--- Week 16 Extra Credit/src/racing.java
+++ Week 16 Extra Credit/src/racing.java
@@ -4,128 +4,59 @@ public class racing {
 
 	public static void main(String[] args) throws Exception {
 
-		Automobile Truck = new Automobile(1999, "Toyota", "Tacoma", "Blue", "Trucky", 3); // creates
-																							// new
-																							// instance
-																							// of
-																							// automobile
-																							// called
-																							// Truck
-		Automobile Van = new Automobile(2001, "Toyota", "Sienna", "Burgendy", "Vanny", 4); // creates
-																							// new
-																							// instance
-																							// of
-																							// automobile
-																							// called
-																							// Van
-		Automobile Sportcar = new Automobile(1996, "Toyota", "Celica", "Red", "Sporty", 6); // creates
-																							// new
-																							// instance
-																							// of
-																							// automobile
-																							// called
-																							// Sportcar
-
+		/*creates new instance of automobile called Truck*/
+		Automobile Truck = new Automobile(1999, "Toyota", "Tacoma", "Blue", "Trucky", 3); 
+		
+		/*creates new instance of automobile called Van*/
+		Automobile Van = new Automobile(2001, "Toyota", "Sienna", "Burgendy", "Vanny", 4);
+		
+		/*creates new instance of automobile called Sportcar*/
+		Automobile Sportcar = new Automobile(1996, "Toyota", "Celica", "Red", "Sporty", 6);
+		
+	
 		Scanner input = new Scanner(System.in); // new scanner object input
-		System.out.println("How many dice would you like to roll?"); // asks
-																		// user
-																		// for
-																		// input
+		
+		/*asks user for input*/
+		System.out.println("How many dice would you like to roll?"); 
+		
+		
 		int choice = input.nextInt(); // assigns user input to variable choice
 
-		int RaceCycles = Dice.Throw(choice); // assigns dice roll of 2 dice to
-												// RaceCycles
+		/*assigns dice roll of user choice dice to RaceCycles*/
+		int RaceCycles = Dice.Throw(choice);
+		
 		String RaceStatus = ""; // declares variable RaceStatus
 
-		for (int cycle = 1; cycle <= RaceCycles; cycle++) {
+		for (int cycle = 1; cycle <= RaceCycles; cycle++) { //loop start
+			
+			try{
+			
+				/*sets operating cycles based on dice roll for 3 automobiles*/
 			Truck.setOperatingCycle(cycle);
 			Van.setOperatingCycle(cycle);
 			Sportcar.setOperatingCycle(cycle);
-		}
-		long startTime = System.currentTimeMillis();
-		long patience = 1000 * 60 * 60;
 		
+			//end for loop
+			} catch (Exception e) {  //prints yellow flag warning if exception is thrown
+				System.out.println("Yellow Flag Warning!");
+			}
+		}
 		Thread t1 = new Thread(Truck);
 		t1.start();
 
-		while (t1.isAlive()) {
-			t1.join(1000);
-			if (((System.currentTimeMillis() - startTime) > patience) && t1.isAlive()) {
-				t1.interrupt();
-				// Shouldn't be long now
-				// -- wait indefinitely
-				t1.join();
-			}
+		Thread t2 = new Thread(Van);
+		t2.start();
 
-			Thread t2 = new Thread(Van);
-			t2.start();
+		Thread t3 = new Thread(Sportcar);
+		t3.start();
 
-			Thread t3 = new Thread(Sportcar);
-			t3.start();
-
-			while (t2.isAlive()) {
-				t2.join(1000);
-				if (((System.currentTimeMillis() - startTime) > patience) && t1.isAlive()) {
-					t2.interrupt();
-					// Shouldn't be long now
-					// -- wait indefinitely
-					t2.join();
-				}
-				System.out.println("");
-				float carSum = (Truck.getDistanceTraveled() + Van.getDistanceTraveled() + Sportcar.getDistanceTraveled());
-				System.out.println("Total vehicle distance = " + carSum + ".");
+		System.out.println("");
+		
+		float carSum = (Truck.getDistanceTraveled() + Van.getDistanceTraveled() + Sportcar.getDistanceTraveled());
+		
+		System.out.println("Total distance = " + carSum + ".");
 				
-				try {
-					for (int cycle = 1; cycle <= RaceCycles; cycle++) { // loops
-																		// based
-																		// on
-																		// operating
-																		// cycle
-
-						// Truck.setOperatingCycle(cycle);
-						// Van.setOperatingCycle(cycle);
-						// Sportcar.setOperatingCycle(cycle);
-
-						RaceStatus = Truck.getName() + ":" + " Current gear = " + Truck.getGear() + " Current speed = "
-								+ Truck.getSpeed() + "\t";
-
-						RaceStatus += Van.getName() + ":" + " Current gear = " + Van.getGear() + " Current speed = "
-								+ Van.getSpeed() + "\t";
-
-						RaceStatus += Sportcar.getName() + ":" + " Current gear = " + Sportcar.getGear()
-								+ " Current speed = " + Sportcar.getSpeed() + "\t";
-
-						System.out.println("Race Cycle: " + cycle);
-						System.out.println(RaceStatus);
-					} // end loop
-
-					/*
-					 * assigns names of objects and distance traveled of each to
-					 * RaceStatus
-					 */
-					float sum = Sportcar.getDistanceTraveled() + Truck.getDistanceTraveled()
-							+ Van.getDistanceTraveled();
-					RaceStatus = Truck.getName() + ":" + " Distance Traveled = " + Truck.getDistanceTraveled() + "\t";
-					RaceStatus += Van.getName() + ":" + " Distance Traveled = " + Van.getDistanceTraveled() + "\t";
-					RaceStatus += Sportcar.getName() + ":" + " Distance Traveled = " + Sportcar.getDistanceTraveled()
-							+ "\t";
-					RaceStatus += "Total Distance of all vehicles = " + sum;
-
-				} catch (Exception e) {
-					System.out.println("Yellow Flag Warning!");
-				}
-
-				/* prints distance of each automobile */
-				System.out.println("\n" + RaceStatus);
-
-				/* prints info of Object based on toString method */
-				System.out.println(Truck.toString());
-				System.out.println(Van.toString());
-				System.out.println(Sportcar.toString());
-				System.out.println("\nGoodbye!");
+		System.out.println("\nGoodbye!");
 			}
-
-		}
-
-	}
 }
+	
